3784d67c35d0ed24d6942cacc781832a4620287b
dimvrontos <102661498+dimvrontos@users.noreply.github.com>
2025-05-26 17:00:19 +0300

    Other users' profile

---
+++ app/src/main/java/com/example/notedapp/OtherUsersActivity.java
@@ -0,0 +1,44 @@
+package com.example.notedapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class OtherUsersActivity extends AppCompatActivity {
+    private Button followbutton;
+    private ImageView profilepicture;
+    private EditText username;
+    private NavigationView navigationView;
+    private ImageButton menuButton;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        followbutton = findViewById(R.id.button);
+        profilepicture = findViewById(R.id.imageView4);
+        username = findViewById(R.id.editTextText);
+        navigationView = findViewById(R.id.fab_search);
+        menuButton = findViewById(R.id.menuButton);
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_other_users);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
